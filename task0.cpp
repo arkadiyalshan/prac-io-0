9ce7baf56a22bb23f3afe0fd7ee21e42bf68fe72
arkadiyalshan <a4spam7@yandex.ru>
2016-10-07 11:16:22 +0300

    Add files via upload

---
+++ task0.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by admin on 07/10/2016.
+//  Copyright © 2016. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
